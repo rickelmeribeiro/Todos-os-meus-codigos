eba3a0777e9998cade48993776815f389db60c1b
rickelmeribeiro <157402554+rickelmeribeiro@users.noreply.github.com>
2024-05-28 16:08:43 -0300

    Update indexjs.js

--- funcoes/indexjs.js
+++ funcoes/indexjs.js
@@ -1,3 +1,22 @@
+const inputs = document.querySelectorAll('.input');
+
+function focusFunc(){
+    let parent = this.parentNode.parentNode;
+    parent.classList.add('focus');
+}
+
+function blurFunc(){
+    let parent = this.parentNode.parentNode;
+    if(this.value == ""){
+        parent.classList.remove('focus');
+    }
+}
+
+inputs.forEach(input => {
+    input.addEventListener('focus', focusFunc);
+    input.addEventListener('blur', blurFunc);
+});
+
 function mostrarsenha() {
     var inputPass = document.getElementById('senha');
     var iconeOlho = document.getElementById('iconeOlho');
@@ -62,7 +81,7 @@ function fazerLogin() {
                     mostrarProcessando();
                 }, 900);
                 setTimeout(function () {
-                    window.location.href = "dashboard.php";
+                    window.location.href = "logadoAdm.php";
                 }, 4000);
             } else {
                 alertlog.style.display = "block";
@@ -73,6 +92,10 @@ function fazerLogin() {
         .catch((error) => {
             console.error("Erro na requisição", error);
         });
+}
+
+function redireciona(page) {
+    window.location.href = page
 }
 
 function mostrarProcessando() {
@@ -92,7 +115,7 @@ function mostrarProcessando() {
     divProcessando.style.top = "50%";
     divProcessando.style.left = "50%";
     divProcessando.style.transform = "translate(-50%, -50%)";
-    divProcessando.innerHTML = '<img src="./img/loadin.gif" width="200" alt="Processando..." title="Processando...">';
+    divProcessando.innerHTML = '<img src="./img/loading/loadin.gif" width="200" alt="Processando..." title="Processando...">';
     document.body.appendChild(divProcessando);
 }
 
@@ -103,13 +126,102 @@ function esconderProcessando() {
     }
 }
 
-function btnEnter() {
+function fazerLoginCliente() {
+    var email = document.getElementById("email").value;
+    var senha = document.getElementById("senha").value;
+
+    var alertlog = document.getElementById("alertlog");
+
+    if (email === "") {
+        alertlog.style.display = "block";
+        alertlog.innerHTML =
+            "Email não digitado.";
+        return;
+    } else if (senha === "") {
+        alertlog.style.display = "block";
+        alertlog.innerHTML =
+            "Senha não digitada.";
+        return;
+    } else if (senha.length < 8) {
+        alertlog.style.display = "block";
+        alertlog.innerHTML = "A senha precisa ter 8 dígitos";
+        return;
+    } else {
+        alertlog.style.display = "none";
+    }
+    fetch("loginCliente.php", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body:
+            "email=" +
+            encodeURIComponent(email) +
+            "&senha=" +
+            encodeURIComponent(senha),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.success) {
+
+                // alert(data.message);
+                alertlog.classList.remove("erroBonito");
+                alertlog.classList.add("acertoBonito");
+                alertlog.innerHTML = data.message;
+                alertlog.style.display = "block";
+                setTimeout(function () {
+                    mostrarProcessando();
+                }, 900);
+                setTimeout(function () {
+                    window.location.href = "logado.php";
+                }, 4000);
+            } else {
+                alertlog.style.display = "block";
+                alertlog.innerHTML = data.message;
+            }
+            // esconderProcessando();
+        })
+        .catch((error) => {
+            console.error("Erro na requisição", error);
+        });
 }
 
-    document.getElementById('senha').addEventListener('keydown', function (event) {
-        if (event.key === 'Enter') {
-            document.getElementById('btn-entere').click();
-        }
-    });
+function ValidaCPF(){
+    var RegraValida=document.getElementById("cpf").value;
+    var cpfValido = /^(([0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2})|([0-9]{11}))$/;
+    if (cpfValido.test(RegraValida) === true)	{
+        console.log("CPF Válido");
+    } else	{
+        console.log("CPF Inválido");
+    }
+}
+function fMasc(objeto,mascara) {
+    obj=objeto
+    masc=mascara
+    setTimeout("fMascEx()",1)
+}
 
+function fMascEx() {
+    obj.value=masc(obj.value)
+}
+function mCPF(cpf) {
+    cpf = cpf.replace(/\D/g, "")
+    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
+    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
+    cpf = cpf.replace(/(\d{3})(\d{1,2})$/, "$1-$2")
+    return cpf
+}
 
+if (document.getElementById("cpf")) {
+    document.getElementById("cpf");
+}
+
+function senhaCadastro(){
+    var validarSenha = document.getElementById("senha").value;
+    var senhaValida = length === 8
+    if (senhaValida.test(validarSenha) === true) {
+        console.log('SENHA VÁLIDA')
+    } else {
+        console.log('A SENHA PRECISA TER 8 DÍGITOS')
+    }
+}
